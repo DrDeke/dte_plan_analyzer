008bd8123d08403d037868b68d8c4dcc817c5231
DrDeke <rdekema@gmail.com>
2021-02-09 17:02:27 -0500

    Initial commit

---
+++ calc.pl
@@ -0,0 +1,205 @@
+#!/usr/bin/perl -w
+
+use DateTime;
+
+my $weekday_peak_first_hr = 11;
+my $weekday_peak_last_hr = 18; #The 6pm-o'clock hour. Off-peak starts at 7pm-o'clock
+
+my $summer_first_mo = 6;
+my $summer_last_mo = 10;
+
+my $winter_peak_rate = 20.213;
+my $winter_offpeak_rate = 11.82;
+my $summer_peak_rate = 22.713;
+my $summer_offpeak_rate = 12.032;
+
+my $standard_rate_tier1 = 15.287;
+my $standard_rate_tier2 = 17.271;
+
+my $std_kwh_today = 0;
+
+my $file = "input.csv";
+
+my $std_tier1_kwh = 0;
+my $std_tier2_kwh = 0;
+
+my $winter_peak_kwh = 0;
+my $winter_offpeak_kwh = 0;
+my $summer_peak_kwh = 0;
+my $summer_offpeak_kwh = 0;
+
+my ($date, $year, $time, $hour, $ampm, $month, $day, $usage, $dayofweek);
+
+# Note: dayofweek 1=Monday, 7=Sunday
+
+open (my $data, '<', $file) or die "Could not open input file.\n";
+
+while (my $line = <$data>)
+{
+	#print "$line\n";
+	my @fields = split "," , $line;
+	$date = $fields[1];
+	$time = $fields[2];
+	$usage = $fields[3];
+
+	$date =~ s/\"//g;
+	$time =~ s/\"//g;
+	$usage =~ s/\"//g;
+
+	@fields = split "/", $date;
+	$month = $fields[0];
+	$day = $fields[1];
+	$year = $fields[2];
+
+	@fields = split ":", $time;
+	$hour = $fields[0];
+
+	@fields = split " ", $time;
+	$ampm = $fields[1];
+
+	if (($ampm eq "PM") && ($hour != 12))
+	{
+		$hour = $hour + 12;
+	}
+
+	if (($ampm eq "AM") && ($hour == 12))
+	{
+		$hour = 0;
+	}
+
+	my $dt = DateTime->new(
+		year => $year,
+		month => $month,
+		day => $day,
+	);
+	
+	$dayofweek = $dt->day_of_week;
+
+
+	#print ">$date< >$hour< >$usage<\n";
+	#print ">$month< >$day< >$year< >$time< >$hour< >$ampm< >$usage< >$dayofweek<\n";
+
+
+	#Accumulate standard (D1) usage
+	if ($hour == 23)
+	{
+		if ($std_kwh_today <= 17)
+		{
+			$std_tier1_kwh = $std_tier1_kwh + $usage;
+			$std_kwh_today = 0;
+			#print "DEBUG: Adding $usage kWh to std_tier1\n";
+			#print "DEBUG: Resetting daily accumulation\n";
+		}
+		else
+		{
+			$std_tier2_kwh = $std_tier2_kwh + $usage;
+			$std_kwh_today = 0;
+			#print "DEBUG: Adding $usage kWh to std_tier2\n";
+			#print "DEBUG: Resetting daily accumulation\n";
+		}
+	}
+	else
+	{
+		if ($std_kwh_today <= 17)
+		{
+			$std_tier1_kwh = $std_tier1_kwh + $usage;
+			$std_kwh_today = $std_kwh_today + $usage;
+                        #print "DEBUG: Adding $usage kWh to std_tier1\n";
+		}
+		else
+		{
+			$std_tier2_kwh = $std_tier2_kwh + $usage;
+			$std_kwh_today = $std_kwh_today + $usage; #unnecessary
+			#print "DEBUG: Adding $usage kWh to std_tier2\n";
+		}
+	}
+
+	#Accumulate time-of-day (D1.2) usage
+	if (($month <= $summer_first_mo) || ($month >= $summer_last_mo)) #It's winter
+	{
+		#print "DEBUG: Rate: Winter    ";
+		if (($dayofweek == 6) || ($dayofweek == 7)) #It's a weekend
+		{
+			#print "Adding $usage kWh to off-peak (weekend)\n";
+			$winter_offpeak_kwh = $winter_offpeak_kwh + $usage;
+		}
+		else
+		{
+			if (($hour < $weekday_peak_first_hr) || ($hour > $weekday_peak_last_hr))
+			{
+				#print "Adding $usage kWh to off-peak (weekday)\n";
+				$winter_offpeak_kwh = $winter_offpeak_kwh + $usage;
+			}
+			else
+			{
+				#print "Adding $usage kWh to on-peak\n";
+				$winter_peak_kwh = $winter_peak_kwh + $usage;
+			}
+		}
+	}
+	else #It's summer
+	{
+                #print "DEBUG: Rate: Summer    ";
+                if (($dayofweek == 6) || ($dayofweek == 7)) #It's a weekend
+                {
+                        #print "Adding $usage kWh to off-peak (weekend)\n";
+                        $summer_offpeak_kwh = $summer_offpeak_kwh + $usage;
+                }
+                else
+                {
+                        if (($hour < $weekday_peak_first_hr) || ($hour > $weekday_peak_last_hr))
+                        {
+                                #print "Adding $usage kWh to off-peak (weekday)\n";
+                                $summer_offpeak_kwh = $summer_offpeak_kwh + $usage;
+                        }
+                        else
+                        {
+                                #print "Adding $usage kWh to on-peak\n";
+                                $summer_peak_kwh = $summer_peak_kwh + $usage;
+                        }
+                }
+	}
+
+}
+
+close ($data);
+
+my $std_total_kwh = int($std_tier1_kwh + $std_tier2_kwh);
+my $std_tier1_dollars = int(($std_tier1_kwh * $standard_rate_tier1) / 100);
+my $std_tier2_dollars = int(($std_tier2_kwh * $standard_rate_tier2) / 100);
+my $std_total_dollars = int($std_tier1_dollars + $std_tier2_dollars);
+$std_tier1_kwh = int($std_tier1_kwh);
+$std_tier2_kwh = int($std_tier2_kwh);
+
+
+
+my $summer_peak_dollars = int(($summer_peak_kwh * $summer_peak_rate) / 100);
+my $winter_peak_dollars = int(($winter_peak_kwh * $winter_peak_rate) / 100);
+my $summer_offpeak_dollars = int(($summer_offpeak_kwh * $summer_offpeak_rate) / 100);
+my $winter_offpeak_dollars = int(($winter_offpeak_kwh * $winter_offpeak_rate) / 100);
+my $total_tod_kwh = int($summer_peak_kwh + $summer_offpeak_kwh + $winter_peak_kwh + $winter_offpeak_kwh);
+my $total_tod_dollars = int($summer_peak_dollars + $summer_offpeak_dollars + $winter_peak_dollars + $winter_offpeak_dollars);
+$summer_peak_kwh = int($summer_peak_kwh);
+$summer_offpeak_kwh = int($summer_offpeak_kwh);
+$winter_peak_kwh = int($winter_peak_kwh);
+$winter_offpeak_kwh = int($winter_offpeak_kwh);
+
+
+print "\n\n";
+print "---Standard D1 Plan---\n";
+print "Tier 1 kWh: $std_tier1_kwh Cost: \$$std_tier1_dollars\n";
+print "Tier 2 kWh: $std_tier2_kwh Cost: \$$std_tier2_dollars\n";
+print "Total  kWh: $std_total_kwh Cost: \$$std_total_dollars\n";
+
+print "\n";
+print "---Time-of-Day D1.2 Plan---\n";
+print "Summer Peak     kWh: $summer_peak_kwh Cost: \$$summer_peak_dollars\n";
+print "Summer Off-Peak kWh: $summer_offpeak_kwh Cost: \$$summer_offpeak_dollars\n";
+print "Winter Peak     kWh: $winter_peak_kwh Cost: \$$winter_peak_dollars\n";
+print "Winter Off-Peak kWh: $winter_offpeak_kwh Cost: \$$winter_offpeak_dollars\n";
+print "Total           kWh: $total_tod_kwh Cost: \$$total_tod_dollars\n";
+
+
+
+
+
