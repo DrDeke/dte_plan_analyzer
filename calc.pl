22c9c08de1157fa201d7218d527cb90f0fd2194a
Rusty Dekema <rdekema@gmail.com>
2022-01-26 18:22:39 -0500

    Add logic for proposed D1.12 rate plan

--- calc.pl
+++ calc.pl
@@ -9,7 +9,7 @@ my $d1debug = 0;
 my $d1_2debug = 0;
 my $d1_8debug = 0;
 my $d1_11debug = 0;
-my $d1_12debug = 1;
+my $d1_12debug = 0;
 
 # Constants
 
@@ -44,7 +44,15 @@ my $d1_11_distrib_rate = 8.194;
 
 # Constants for Proposed D1.12 rates
 
+my $d1_12_winter_peak_noncap_rate = 5.157;
+my $d1_12_winter_offpeak_noncap_rate = 4.740;
+my $d1_12_summer_peak_noncap_rate = 6.432;
+my $d1_12_summer_offpeak_noncap_rate = 4.740;
 
+my $d1_12_summer_first_mo = 6;
+my $d1_12_summer_last_mo = 9;
+my $d1_12_weekday_peak_first_hr = 15;
+my $d1_12_weekday_peak_last_hr = 18; #The 6pm-o'clock Hour. Off-peak starts at 7pm-o'clock
 
 
 # Regular vars
@@ -78,10 +86,12 @@ my $d1_12_demandhr1_hour = -1;
 my $d1_12_demandhr2_hour = -1;
 my $d1_12_demandhr3_hour = -1;
 
+my $d1_12_demand_kw;
+my $d1_12_demand_level;
+my $d1_12_capacity_dollars;
+my $d1_12_delivery_dollars;
 
-
-
-
+my ($d1_12_winter_peak_kwh, $d1_12_winter_offpeak_kwh, $d1_12_summer_peak_kwh, $d1_12_summer_offpeak_kwh);
 
 
 my ($date, $year, $time, $hour, $ampm, $month, $day, $usage, $dayofweek);
@@ -119,6 +129,7 @@ open (my $data, '<', $file) or die "Could not open input file $file.\n";
 
 # Get top three usage hours per proposed D1.12 rules
 # (which have some complications, so look them up if needed)
+# (at https://mi-psc.force.com/sfc/servlet.shepherd/version/download/0688y000001oeWBAAY )
 
 my $d1_12_demand_rows = `cat $file | sort -g -r --field-separator=',' -k 4,4 |awk -F',' '{ if(!a[\$2]++){print} }' | head -3`;
 my @d1_12_fields = split ",", $d1_12_demand_rows;
@@ -135,6 +146,8 @@ $d1_12_demandhr1_hour = $d1_12_fields[2];
 $d1_12_demandhr2_hour = $d1_12_fields[7];
 $d1_12_demandhr3_hour = $d1_12_fields[12];
 
+$d1_12_demand_kw = ($d1_12_demandhr1_kwh + $d1_12_demandhr2_kwh + $d1_12_demandhr3_kwh) / 3;
+$d1_12_demand_level = int($d1_12_demand_kw);
 
 if ($d1_12debug == 1)
 {
@@ -145,13 +158,87 @@ if ($d1_12debug == 1)
 	print "DEBUG_D1_12: Demand Hour 1: $d1_12_demandhr1_date $d1_12_demandhr1_hour $d1_12_demandhr1_kwh kWh\n";
 	print "DEBUG_D1_12: Demand Hour 2: $d1_12_demandhr2_date $d1_12_demandhr2_hour $d1_12_demandhr2_kwh kWh\n";
 	print "DEBUG_D1_12: Demand Hour 3: $d1_12_demandhr3_date $d1_12_demandhr3_hour $d1_12_demandhr3_kwh kWh\n";
+	print "DEBUG_D1_12: Demand kW: $d1_12_demand_kw\n";
 
 }
 
+# Compute capacity energy charge and delivery charge for D1.12
+if ($d1_12_demand_kw < 1)
+{
+	$d1_12_capacity_dollars = 0;
+}
+elsif (($d1_12_demand_kw >= 1) && ($d1_12_demand_kw < 2))
+{
+	$d1_12_capacity_dollars = 5.85;
+}
+elsif (($d1_12_demand_kw >= 2) && ($d1_12_demand_kw < 3))
+{
+	$d1_12_capacity_dollars = 11.70;
+}
+elsif (($d1_12_demand_kw => 3) && ($d1_12_demand_kw < 9))
+{
+	$d1_12_capacity_dollars = 17.56 + 5.85 * (int($d1_12_demand_kw) - 3)
+}
+else
+{
+	$d1_12_capacity_dollars = 52.67 + 5.85 * ($d1_12_demand_kw - 9);
+}
 
-exit 1;
+if ($d1_12debug == 1)
+{
+	print "DEBUG_D1_12: Energy Capacity Dollars: $d1_12_capacity_dollars\n";
+}
 
-# Read and parse readings in the input file
+if ($d1_12_demand_kw < 1)
+{
+	$d1_12_delivery_dollars = 0;
+}
+elsif (($d1_12_demand_kw >= 1) && ($d1_12_demand_kw < 2))
+{
+	$d1_12_delivery_dollars = 10.76;
+}
+elsif (($d1_12_demand_kw >= 2) && ($d1_12_demand_kw < 3))
+{
+        $d1_12_delivery_dollars = 21.51;
+}
+elsif (($d1_12_demand_kw >= 3) && ($d1_12_demand_kw < 4))
+{
+        $d1_12_delivery_dollars = 32.27;
+}
+elsif (($d1_12_demand_kw >= 4) && ($d1_12_demand_kw < 5))
+{
+        $d1_12_delivery_dollars = 43.02;
+}
+elsif (($d1_12_demand_kw >= 5) && ($d1_12_demand_kw < 6))
+{
+        $d1_12_delivery_dollars = 53.78;
+}
+elsif (($d1_12_demand_kw >= 6) && ($d1_12_demand_kw < 7))
+{
+        $d1_12_delivery_dollars = 64.53;
+}
+elsif (($d1_12_demand_kw >= 7) && ($d1_12_demand_kw < 8))
+{
+        $d1_12_delivery_dollars = 75.29;
+}
+elsif (($d1_12_demand_kw >= 8) && ($d1_12_demand_kw < 9))
+{
+        $d1_12_delivery_dollars = 86.04;
+}
+else
+{
+	$d1_12_delivery_dollars = 96.80 + 10.76 * ($d1_demand_kw - 9);
+}
+
+if ($d1_12debug == 1)
+{
+	print "DEBUG_D1_12: Delivery Dollars: $d1_12_delivery_dollars\n";
+}
+
+
+
+
+# Read and parse hourly usage data from the input file
 while (my $line = <$data>)
 {
 	if ($general_debug0 == 1)
@@ -407,6 +494,76 @@ while (my $line = <$data>)
 		}
 	}
 
+	#Accumulate D1.12 (proposed) non-capacity energy usage
+	if (($month < $d1_12_summer_first_mo) || ($month > $d1_12_summer_last_mo)) #It's winter
+	{
+		if ($d1_12debug == 1)
+		{
+			print "DEBUG_D1_12: Rate: Winter    ";
+		}
+		if (($dayofweek == 6) || ($dayofweek == 7)) #It's a weekend
+		{
+			if ($d1_12debug == 1)
+			{
+				print "Adding $usage kWh to winter off-peak (weekend)\n";
+			}
+			$d1_12_winter_offpeak_kwh = $d1_12_winter_offpeak_kwh + $usage;
+		}
+		else
+		{
+			if (($hour < $d1_12_weekday_peak_first_hr) || ($hour > $d1_12_weekday_peak_last_hr))
+			{
+				if ($d1_12debug == 1)
+				{
+					print "Adding $usage kWh to winter off-peak (weekday)\n";
+				}
+				$d1_12_winter_offpeak_kwh = $d1_12_winter_offpeak_kwh + $usage;
+			}
+			else
+			{
+				if ($d1_12debug == 1)
+				{
+					print "Adding $usage kWh to winter on-peak\n";
+				}
+				$d1_12_winter_peak_kwh = $d1_12_winter_peak_kwh + $usage;
+			}
+		}
+	}
+	else #It's summer
+	{
+		if ($d1_12debug == 1)
+		{
+			print "DEBUG_D1_12: Rate: Summer    ";
+		}
+		if (($dayofweek == 6) || ($dayofweek == 7)) #It's a weekend
+		{
+			if ($d1_12debug == 1)
+			{
+				print "Adding $usage kWh to summer off-peak (weekend)\n";
+			}
+			$d1_12_summer_offpeak_kwh = $d1_12_summer_offpeak_kwh + $usage;
+		}
+		else
+		{
+			if (($hour < $d1_12_weekday_peak_first_hr) || ($hour > $d1_12_weekday_peak_last_hr))
+			{
+				if ($d1_12debug == 1)
+				{
+					print "Adding $usage kWh to summer off-peak (weekday)\n";
+				}
+				$d1_12_summer_offpeak_kwh = $d1_12_summer_offpeak_kwh + $usage;
+			}
+			else
+			{
+				if ($d1_12debug == 1)
+				{
+					print "Adding $usage kWh to summer on-peak (weekday)\n";
+				}
+				$d1_12_summer_peak_kwh = $d1_12_summer_peak_kwh + $usage;
+			}
+		}
+	}
+
 }
 
 close ($data);
@@ -442,6 +599,25 @@ $d1_11_winter_peak_kwh = int($d1_11_winter_peak_kwh);
 $d1_11_summer_offpeak_kwh = int($d1_11_summer_offpeak_kwh);
 $d1_11_summer_peak_kwh = int($d1_11_summer_peak_kwh);
 
+my $d1_12_delivery_dollars_year = int($d1_12_delivery_dollars * 12);
+my $d1_12_capacity_dollars_year = int($d1_12_capacity_dollars * 12);
+
+my $d1_12_winter_peak_dollars = int(($d1_12_winter_peak_noncap_rate * $d1_12_winter_peak_kwh) / 100);
+my $d1_12_winter_offpeak_dollars = int(($d1_12_winter_offpeak_noncap_rate * $d1_12_winter_offpeak_kwh) / 100);
+my $d1_12_summer_peak_dollars = int(($d1_12_summer_peak_noncap_rate * $d1_12_summer_peak_kwh) / 100);
+my $d1_12_summer_offpeak_dollars = int(($d1_12_summer_offpeak_noncap_rate * $d1_12_summer_offpeak_kwh) / 100);
+
+my $d1_12_total_kwh = int($d1_12_winter_peak_kwh + $d1_12_winter_offpeak_kwh + $d1_12_summer_peak_kwh + $d1_12_summer_offpeak_kwh);
+my $d1_12_total_noncap_energy_dollars = $d1_12_winter_peak_dollars + $d1_12_winter_offpeak_dollars + $d1_12_summer_peak_dollars + $d1_12_summer_offpeak_dollars;
+
+$d1_12_winter_peak_kwh = int($d1_12_winter_peak_kwh);
+$d1_12_winter_offpeak_kwh = int($d1_12_winter_offpeak_kwh);
+$d1_12_summer_peak_kwh = int($d1_12_summer_peak_kwh);
+$d1_12_summer_offpeak_kwh = int($d1_12_summer_offpeak_kwh);
+
+my $d1_12_grand_total_dollars = $d1_12_delivery_dollars_year + $d1_12_capacity_dollars_year + $d1_12_winter_peak_dollars + $d1_12_winter_offpeak_dollars + $d1_12_summer_peak_dollars + $d1_12_summer_offpeak_dollars;
+
+
 if ($d1_12debug == 1)
 {
 	print "DEBUG_D1_12: TopHour1 date=$d1_12_demandhr1_date hour=$d1_12_demandhr1_hour kwh=$d1_12_demandhr1_kwh\n";
@@ -471,5 +647,24 @@ print "Summer Off-Peak kWh: $d1_11_summer_offpeak_kwh Cost: \$$d1_11_summer_offp
 print "Winter Peak     kWh: $d1_11_winter_peak_kwh Cost: \$$d1_11_winter_peak_dollars\n";
 print "Winter Off-Peak kWh: $d1_11_winter_offpeak_kwh Cost: \$$d1_11_winter_offpeak_dollars\n";
 print "Total           kWh: $d1_11_total_kwh Cost: \$$d1_11_total_dollars\n";
+
+print "\n";
+print "---Proposed Time-of-Day With Demand Charge D1.12 Plan (U-20836)---\n";
+print "Demand Hour 1: $d1_12_demandhr1_date $d1_12_demandhr1_hour $d1_12_demandhr1_kwh kWh\n";
+print "Demand Hour 2: $d1_12_demandhr2_date $d1_12_demandhr2_hour $d1_12_demandhr2_kwh kWh\n";
+print "Demand Hour 3: $d1_12_demandhr3_date $d1_12_demandhr3_hour $d1_12_demandhr3_kwh kWh\n";
+print "Service Level (demand): $d1_12_demand_level\n\n";
+
+print "Delivery Charge (demand): \$$d1_12_delivery_dollars_year\n";
+print "Capacity Energy Charge (demand): \$$d1_12_capacity_dollars_year\n\n";
+
+print "Non-Capacity Energy Charges (usage):\n";
+print "Summer Peak     kWh: $d1_12_summer_peak_kwh Cost: \$$d1_12_summer_peak_dollars\n";
+print "Summer Off-Peak kWh: $d1_12_summer_offpeak_kwh Cost: \$$d1_12_summer_offpeak_dollars\n";
+print "Winter Peak     kWh: $d1_12_winter_peak_kwh Cost: \$$d1_12_winter_peak_dollars\n";
+print "Winter Off-Peak kWh: $d1_12_winter_offpeak_kwh Cost: \$$d1_12_winter_offpeak_dollars\n";
+print "    Subtotal Non-Capacity Energy kWh: $d1_12_total_kwh Cost: \$$d1_12_total_noncap_energy_dollars\n\n";
+print "Total D1.12 Cost: \$$d1_12_grand_total_dollars\n\n";
+
 
 
